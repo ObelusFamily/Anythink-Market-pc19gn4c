c1ae17eb6cfd7405c88fe76da6c049fc1ce0a238
Si Young Byun <70714904+siyoungbyun@users.noreply.github.com>
2022-12-16 19:18:47 +0000

    task: in progress

--- frontend/src/components/Home/Banner.js
+++ frontend/src/components/Home/Banner.js
@@ -1,7 +1,20 @@
 import React from "react";
+import agent from "../../agent";
 import logo from "../../imgs/logo.png";
 
-const Banner = () => {
+const Banner = (props) => {
+
+  const handleSearch = (event) => {
+    const term = event.target.value;
+    if (term.length >= 3) {
+      props.onSearchInput(
+        term,
+        (page) => agent.Items.byTitle(term, page),
+        agent.Items.byTitle(term)
+      );
+    }
+  }
+
   return (
     <div className="banner text-white">
       <div className="container p-4 text-center">
@@ -9,6 +22,26 @@ const Banner = () => {
         <div>
           <span>A place to </span>
           <span id="get-part">get</span>
+          <span>
+            <form style={{borderRadius: "5px", margin: "10px", background: "white", width: "40%", display: "inline-block"}}>
+              <input
+                onInput={handleSearch}
+                type="search"
+                placeholder="What is it that you truly desire?"
+                style={{
+                  // border: "1px solid #555",
+                  width: "100%",
+                  margin: 0,
+                  padding: "9px 50px 9px 10px",
+                  fontSize: "14px",
+                  background: 'transparent url("https://img.icons8.com/ios-glyphs/30/null/search--v1.png") no-repeat 330px center',
+                  color: "black",
+                  border: "none",
+                  borderRadius: "inherit",
+                }}
+              />
+            </form>
+          </span>
           <span> the cool stuff.</span>
         </div>
       </div>
