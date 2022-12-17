cc442fe5a6583d0d4ca5ee9abfd5cab132b99446
Si Young Byun <siyoung.byun@gmail.com>
2022-12-17 01:13:28 -0500

    task: Search bar only appears when get clicked

--- frontend/src/components/Home/Banner.js
+++ frontend/src/components/Home/Banner.js
@@ -3,6 +3,15 @@ import agent from "../../agent";
 import logo from "../../imgs/logo.png";
 
 const Banner = (props) => {
+  const [isSearchBarVisible, setIsSearchBarVisible] = React.useState(false);
+  const [isSearchBarOpaque, setIsSearchBarOpaque] = React.useState(false);
+
+  const handleClickGet = () => {
+    if (isSearchBarVisible === false) {
+      setIsSearchBarVisible(true);
+      setTimeout(() => setIsSearchBarOpaque(true), 2);
+    }
+  };
 
   const handleSearch = (event) => {
     const term = event.target.value;
@@ -19,7 +28,7 @@ const Banner = (props) => {
         agent.Items.all()
       );
     }
-  }
+  };
 
   return (
     <div className="banner text-white">
@@ -27,9 +36,25 @@ const Banner = (props) => {
         <img src={logo} alt="banner" />
         <div>
           <span>A place to </span>
-          <span id="get-part">get</span>
-          <span>
-            <form style={{borderRadius: "5px", margin: "10px", background: "white", width: "365px", display: "inline-block"}}>
+          <span id="get-part" onClick={handleClickGet}>
+            get
+          </span>
+          <span
+            style={{
+              display: isSearchBarVisible ? "inline" : "none",
+              transition: "opacity 1s ease-in",
+              opacity: isSearchBarOpaque ? 1 : 0,
+            }}
+          >
+            <form
+              style={{
+                borderRadius: "5px",
+                margin: "10px",
+                background: "white",
+                width: "365px",
+                display: "inline-block",
+              }}
+            >
               <input
                 id="search-box"
                 onInput={handleSearch}
@@ -40,7 +65,8 @@ const Banner = (props) => {
                   margin: 0,
                   padding: "9px 50px 9px 10px",
                   fontSize: "14px",
-                  background: 'transparent url("https://img.icons8.com/ios-glyphs/30/null/search--v1.png") no-repeat 330px center',
+                  background:
+                    'transparent url("https://img.icons8.com/ios-glyphs/30/null/search--v1.png") no-repeat 330px center',
                   color: "black",
                   border: "none",
                   borderRadius: "inherit",
